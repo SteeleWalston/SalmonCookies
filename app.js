01231e9c29a24110c15e8ab469d47245a8854188
Steele Walston <steele.walston@gmail.com>
2018-02-05 15:11:31 -0800

    have working random numbers for each location

--- app.js
+++ app.js
@@ -22,7 +22,65 @@ const cookiesAirport = {
         const min = this.minCustomers;
         const max = this.maxCustomers;
         return Math.round(Math.random() * (max - min) + min);
+    },
+    createLi: function () {
+        const li = document.createElement('li');
+        li.textContent = Math.round(this.customersPerHour() * this.avgCookiesPerCustomer);
+        return li;
     }
 };
 
 console.log(cookiesAirport.customersPerHour());
+console.log(cookiesAirport.createLi());
+
+const cookiesPioneer = {
+    minCustomers: 3,
+    maxCustomers: 24,
+    avgCookiesPerCustomer: 1.2,
+    customersPerHour: function() {
+        const min = this.minCustomers;
+        const max = this.maxCustomers;
+        return Math.round(Math.random() * (max - min) + min);
+    }
+};
+
+console.log(cookiesPioneer.customersPerHour());
+
+const cookiesPowells = {
+    minCustomers: 11,
+    maxCustomers: 38,
+    avgCookiesPerCustomer: 3.7,
+    customersPerHour: function() {
+        const min = this.minCustomers;
+        const max = this.maxCustomers;
+        return Math.round(Math.random() * (max - min) + min);
+    }
+};
+
+console.log(cookiesPowells.customersPerHour());
+
+const cookiesStJohns = {
+    minCustomers: 20,
+    maxCustomers: 38,
+    avgCookiesPerCustomer: 2.3,
+    customersPerHour: function() {
+        const min = this.minCustomers;
+        const max = this.maxCustomers;
+        return Math.round(Math.random() * (max - min) + min);
+    }
+};
+
+console.log(cookiesStJohns.customersPerHour());
+
+const cookiesWaterFront = {
+    minCustomers: 2,
+    maxCustomers: 16,
+    avgCookiesPerCustomer: 4.6,
+    customersPerHour: function() {
+        const min = this.minCustomers;
+        const max = this.maxCustomers;
+        return Math.round(Math.random() * (max - min) + min);
+    }
+};
+
+console.log(cookiesWaterFront.customersPerHour());
