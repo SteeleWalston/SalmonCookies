fc876c54fe6410a545a0084657525923b147d23c
Steele Walston <steele.walston@gmail.com>
2018-02-06 16:42:59 -0800

    cleaned up code

--- app.js
+++ app.js
@@ -76,25 +76,7 @@ Location.prototype.cookiesPerHour = function() {
     }
 };
 
-// Location.prototype.renderCookies = function() {
-//     for (let i = 0; i < this.hourlyArray.length; i++) {
-//         const airportList = document.getElementById(this.parentId);
-//         const newListItem = document.createElement('li');
-//         newListItem.textContent = timeOfDay[i] + this.hourlyArray[i];
-    
-//         airportList.appendChild(newListItem);
-//     }
-// };
-
-Location.prototype.renderTotal = function() {
-    const airportList = document.getElementById(this.parentId);
-    const newListItem = document.createElement('li');
-    newListItem.textContent = 'Total: ' + this.hourlyArray.reduce(reducer);
-
-    airportList.appendChild(newListItem);
-};
-
-Location.prototype.renderHeader = function() {
+function renderHeader() {
     const thead = document.querySelector('#cookies-table thead');
     const tr = document.createElement('tr');
     for (let i = 0; i < timeOfDay.length; i++) {
@@ -107,7 +89,7 @@ Location.prototype.renderHeader = function() {
     }
 };
 
-Location.prototype.renderFooter = function() {
+function renderFooter() {
     const tfoot = document.querySelector('#cookies-table tfoot');
     const tr = document.createElement('tr');
     for (let i = 0; i < footerArray.length; i++) {
@@ -120,59 +102,47 @@ Location.prototype.renderFooter = function() {
     }
 };
 
-// Location.prototype.renderFooter = function() {
-//     const tfoot = document.querySelector('#cookies-table thead');
-//     const tr = document.createElement('tr');
-//     for (let i = 0; i < timeOfDay.length; i++) {
-//         const td = document.createElement('td');
-//         td.textContent = timeOfDay[i];
-
-//         tr.appendChild(td);
-//         tfoot.appendChild(tr);
-
-//     }
-// };
-
 Location.prototype.buildTable = function() {
     const tbody = document.querySelector('#cookies-table tbody');
     const tr = document.createElement('tr');
     const td = document.createElement('td');
     td.textContent = this.location;
     tr.appendChild(td);
-    for (let i = 0; i < this.hourlyArray.length - 1; i++) {
+    for (let i = 0; i < this.hourlyArray.length - 2; i++) {
         const td = document.createElement('td');
         td.textContent = this.hourlyArray[i];
+        // footerArray.push(i);
+
 
         tr.appendChild(td);
         tbody.appendChild(tr);
     }
+    const th = document.createElement('th');
+    th.textContent = this.hourlyArray.reduce(reducer);
 
+    tr.appendChild(th);
 };
 
 const airportLocation = new Location('Airport', 23, 65, 6.3);
 console.log(airportLocation);
 airportLocation.cookiesPerHour();
-// airportLocation.renderTotal();
-airportLocation.renderHeader();
-airportLocation.renderFooter();
 airportLocation.buildTable();
 
 const pioneerLocation = new Location('Pioneer', 3, 24, 1.2);
 pioneerLocation.cookiesPerHour();
-// pioneerLocation.renderTotal();
 pioneerLocation.buildTable();
 
 const powellsLocation = new Location('Powells', 3, 24, 1.2);
 powellsLocation.cookiesPerHour();
-// powellsLocation.renderTotal();
 powellsLocation.buildTable();
 
 const stjohnsLocation = new Location('St. Johns', 3, 24, 1.2);
 stjohnsLocation.cookiesPerHour();
-// stjohnsLocation.renderTotal();
 stjohnsLocation.buildTable();
 
 const waterfrontLocation = new Location('Waterfront', 3, 24, 1.2);
 waterfrontLocation.cookiesPerHour();
-// waterfrontLocation.renderTotal();
 waterfrontLocation.buildTable();
+
+renderHeader();
+renderFooter();
